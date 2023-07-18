0abac8db2c7564ade077e8fdc388df71c19fb63f
HyunHee-Choi <133738840+HyunHee-Choi@users.noreply.github.com>
2023-07-18 13:20:54 +0900

    commit

---
+++ frontend/src/components/DecresestockCommand.vue
@@ -0,0 +1,61 @@
+<template>
+    <v-card outlined>
+        <v-card-title>
+            Decresestock
+        </v-card-title>
+
+        <v-card-text>
+            <Number label="Qty" v-model="value.qty" :editMode="editMode"/>
+        </v-card-text>
+
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+                    color="primary"
+                    text
+                    @click="decresestock"
+            >
+                Decresestock
+            </v-btn>
+            
+            <v-btn
+                    color="primary"
+                    text
+                    @click="close"
+            >
+                Close
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+
+</template>
+
+<script>
+   
+    export default {
+        name: 'DecresestockCommand',
+        components:{},
+        props: {},
+        data: () => ({
+            editMode: true,
+            value: {},
+        }),
+        created() {
+            this.value.qty = 0;
+        },
+        watch: {
+        },
+        methods: {
+            decresestock() {
+                this.$emit('decresestock', this.value);
+            },
+            close() {
+                this.$emit('closeDialog');
+            },
+            change() {
+                this.$emit('input', this.value);
+            },
+        }
+    }
+</script>
+
